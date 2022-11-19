5ba71971512839f9a65f720b743d9870169777b4
Bartłomiej Rudecki <ejbtrd@gmail.com>
2022-11-19 20:32:09 +0100

    support: Set google-sans as default font with fonts_customization
    Change-Id: I68074937315617bbebe6df6b8c51f1dd494ee2cf

--- common.mk
+++ common.mk
@@ -3,3 +3,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     tequilaThemesStub
+
+PRODUCT_COPY_FILES += \
+    vendor/support/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
